5bca46d76ade5fad5ad0a52b46f437c176341f75
IraCalvo <ira.zephaniah.calvo@gmail.com>
2024-02-16 23:45:10 -0800

    Worked on player camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] Camera playerCamera;
     [SerializeField] float moveSpeed;
+    [SerializeField] float lookSensitivity;
+
+    public bool eyesOpen = true;
     private Vector2 playerMovement;
+    Vector2 mouseDelta;
     Rigidbody rb;
 
     private void Awake()
     {
+        playerCamera = GetComponent<Camera>();
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
     {
-        
-    }
-
-    private void FixedUpdate()
-    {
         MovePlayer();
+        RotateCamera();
     }
 
     void OnMove(InputValue value)
@@ -32,6 +36,23 @@ public class PlayerMovement : MonoBehaviour
 
     void MovePlayer()
     {
-        rb.velocity = new Vector3(playerMovement.x * moveSpeed, rb.velocity.y, playerMovement.y * moveSpeed);
+        if (eyesOpen == false)
+        {
+            rb.velocity = new Vector3(playerMovement.x * moveSpeed, rb.velocity.y, playerMovement.y * moveSpeed);
+        }
+    }
+
+    void OnLook(InputValue delta)
+    {
+        mouseDelta = delta.Get<Vector2>() * lookSensitivity;
+    }
+
+    void RotateCamera()
+    {
+        //look up and down
+        transform.Rotate(Vector3.up * mouseDelta.x);
+
+        //look left and right
+        playerCamera.transform.Rotate(Vector3.left * mouseDelta);
     }
 }
