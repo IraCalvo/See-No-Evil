3257804446fea1dae20e72010d7de17cce6169dd
RyFranco <rymfranco@gmail.com>
2024-02-16 21:38:09 -0800

    Test uno
    yep

---
+++ Assets/Scripts/YoSwag.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YoSwag : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("swag to the yo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
